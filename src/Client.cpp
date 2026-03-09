7da2772dd6cbed058f220bb10a57b26d1b86976d
Samih <samih.ouague96@gmail.com>
2026-03-09 04:01:04 +0100

    extract cmds in vector container

--- src/Client.cpp
+++ src/Client.cpp
@@ -12,27 +12,28 @@
 
 #include "Client.hpp"
 #include <iostream>
+#include <vector>
 
 Client::Client(void)
 {
 	std::cout << "Client: Default constructor called." << std::endl;
-	return ;
+	return;
 }
 
 Client::Client(Client const &instance)
 {
 	std::cout << "Client: Copy constructor called." << std::endl;
 	*this = instance;
-	return ;
+	return;
 }
 
-int	Client::getFd(void) const
+int Client::getFd(void) const
 {
 	return (*this).fd;
 }
 
-Client&	Client::operator=(Client const &instance)
-{	
+Client &Client::operator=(Client const &instance)
+{
 	std::cout << "Client: Assignation overload called." << std::endl;
 	if (this == &instance)
 		return (*this);
@@ -43,8 +44,7 @@ Client&	Client::operator=(Client const &instance)
 	return (*this);
 }
 
-
-int		Client::acceptConnection(int const &sockfd)
+int Client::acceptConnection(int const &sockfd)
 {
 	(*this).fd = accept(sockfd, (struct sockaddr *)&(*this).addr, (socklen_t *)&(*this).socklen);
 	if ((*this).fd == -1)
@@ -55,36 +55,67 @@ int		Client::acceptConnection(int const &sockfd)
 	return (*this).fd;
 }
 
-void	welcomeMsg(int conn_sock, std::string msg)
+std::vector<std::string> extract_cmd(std::string req)
 {
-	std::string	cursor(msg);
+	std::vector<std::string> argv;
+	std::string cursor(req);
+	long unsigned int bnpos = -1;
+
+	for (long unsigned int spos = cursor.find(' '); spos != std::string::npos; spos = cursor.find(' '))
+	{
+		bnpos = cursor.find(' ');
+		if (bnpos == std::string::npos)
+			break;
+		else
+			argv.push_back(cursor.substr(0, bnpos));
+		cursor = cursor.substr(bnpos + 1);
+	}
+	bnpos = cursor.find('\r');
+	argv.push_back(cursor.substr(0, bnpos));
+	return argv;
+}
+
+void exec_cmd(int conn_sock, std::vector<std::string> argv)
+{
+	std::string welcome;
+	std::string nick;
+
+	if (argv[0] == "NICK")
+	{
+		if (argv.size() != 2)
+			return ;
+			
+		nick = argv[1];
+		welcome = ":localhost 001 " + nick +" :Welcome to the server " + nick + ".\r\n";
+		send(conn_sock, welcome.c_str(), welcome.size(), 0);
+	}
+}
+
+void exec_req(int conn_sock, std::string msg)
+{
+	std::string cursor(msg);
 	std::string cmd("");
-	std::string welcome("");
-	std::string nick("");
-	long unsigned int	bnpos = -1;
+	long unsigned int bnpos = -1;
+	std::vector<std::string> argv;
 
 	while (cursor.find('\n') != std::string::npos)
 	{
 		bnpos = cursor.find('\n');
 		cmd = cursor.substr(0, bnpos);
 		cursor = cursor.substr(bnpos + 1);
-		bnpos = cmd.find(' ');
-		if (bnpos == std::string::npos)
-			continue;
-		else if (cmd.substr(0, bnpos) == std::string("NICK"))
-		{
-			nick = cmd.substr(bnpos + 1, cmd.size() - (bnpos + 2));
-			welcome = ":localhost 001 " + nick + " :Welcome to the server " + nick +".\r\n";
-			send(conn_sock, welcome.c_str(), welcome.size(), 0);
-		}
+		std::cout << std::endl
+				  << "########" << std::endl;
+		std::cout << cmd << std::endl;
+		argv = extract_cmd(cmd);
+		exec_cmd(conn_sock, argv);
 	}
 }
 
-void	Client::readCmd(void)
+void Client::readCmd(void)
 {
 	char buffer[512];
-	std::string	msg("");
-	int	conn_sock = (*this).fd;
+	std::string msg("");
+	int conn_sock = (*this).fd;
 
 	while (true)
 	{
@@ -95,15 +126,14 @@ void	Client::readCmd(void)
 		buffer[bytes] = '\0';
 		msg = msg + std::string(buffer);
 		if (buffer[bytes - 1] == '\n' && buffer[bytes - 2] == '\r')
-			break ;
+			break;
 	}
-	std::cout << msg << std::endl;
-	welcomeMsg(conn_sock, msg);
-	return ;
+	exec_req(conn_sock, msg);
+	return;
 }
 
 Client::~Client(void)
-{	
+{
 	std::cout << "Client: Destructor called." << std::endl;
-	return ;
+	return;
 }
